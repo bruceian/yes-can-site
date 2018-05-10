96ba1ed72228533ffeaa321bc8a59b6febc602d5
Ian Bruce <brucegordonian@gmail.com>
2018-05-10 08:40:22 -0400

    Added mailchimp functionality and page redirects for age

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,73 @@
 //= require_tree .
 
 $(function() {
+    // Activate box-shadow navbar once scrolled
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        var navBar = $('.navbar');
 
+        if(height  > 5) {
+          navBar.addClass('box-shadow');
+        } else {
+          navBar.removeClass('box-shadow');
+        }
+    });
+
+    // Scroll to Top on Yesca logo click
+    $(".yesca-title").on("click",function(){
+        $("html, body").animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+
+    // Check If Email Valid
+    $('#mce-EMAIL').on('input', function() {
+      var input = $(this);
+      var re = /^([A-Za-z0-9_\-\.])+\@(?!(?:[A-Za-z0-9_\-\.]+\.)?([A-Za-z]{2,4})\.\2)([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+      var is_email = re.test(input.val());
+
+      if(is_email) {
+        $(this).removeClass('invalid');
+      } else {
+        $(this).addClass('invalid');
+      }
+    });
+
+    var inputq1 = $('#mce-FNAME');
+    var inputq2 = $('#mce-LNAME');
+    var inputq3 = $('#mce-EMAIL');
+
+    $("#mc-embedded-subscribe").click(function(event) {
+      if ($("#mce-EMAIL").hasClass("invalid")) {
+        event.preventDefault();
+        $('#input-msg').text('Please provide valid email');
+      } else if(inputq1 == '' || inputq2 == '' || inputq3 == ''){
+        event.preventDefault();
+        $('#input-msg').text('Please provide valid email');
+      } else {
+        return true
+      }
+    });
+
+    // Disable Scroll on Pop-up page
+    var popUpPage = $("#pop-up").parent();
+
+    popUpPage.css({
+      "overflow-y":"hidden"
+    });
+
+    // Open and Close Pop-Up
+    $("button.sign-up").click(function () {
+        $('.pop-up').fadeIn(400);
+      });
+
+    $(".close").click(function () {
+      $( "#post_email" ).val("");
+      $('.pop-up').fadeOut(200);
+    });
+
+  });
+
+document.addEventListener('turbolinks:load', function() {
   // Activate box-shadow navbar once scrolled
   $(window).scroll(function() {
       var height = $(window).scrollTop();
@@ -35,13 +101,50 @@ $(function() {
       return false;
   });
 
+  // Check If Email Valid
+  $('#mce-EMAIL').on('input', function() {
+    var input = $(this);
+    var re = /^([A-Za-z0-9_\-\.])+\@(?!(?:[A-Za-z0-9_\-\.]+\.)?([A-Za-z]{2,4})\.\2)([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    var is_email = re.test(input.val());
+
+    if(is_email) {
+      $(this).removeClass('invalid');
+    } else {
+      $(this).addClass('invalid');
+    }
+  });
+
+  var inputq1 = $('#mce-FNAME');
+  var inputq2 = $('#mce-LNAME');
+  var inputq3 = $('#mce-EMAIL');
+
+  $("#mc-embedded-subscribe").click(function(event) {
+    if ($("#mce-EMAIL").hasClass("invalid")) {
+      event.preventDefault();
+      $('#input-msg').text('Please provide valid email');
+    } else if(inputq1 == '' || inputq2 == '' || inputq3 == ''){
+      event.preventDefault();
+      $('#input-msg').text('Please provide valid email');
+    } else {
+      return true
+    }
+  });
+
   // Disable Scroll on Pop-up page
   var popUpPage = $("#pop-up").parent();
 
   popUpPage.css({
     "overflow-y":"hidden"
-  })
+  });
 
+  // Open and Close Pop-Up
+  $("button.sign-up").click(function () {
+      $('.pop-up').fadeIn(400);
+    });
 
+  $(".close").click(function () {
+    $( "#post_email" ).val("");
+    $('.pop-up').fadeOut(200);
+  });
 });
 // END OF JQUERY
